94484c9e07babd2977371db453b0f6fb98638bc5
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-10-12 15:54:36 -0300

    pagina do mapa completa

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/map-marker.svg'
 
@@ -21,7 +24,13 @@ function OrphanagesMap(){
                 </footer>
             </aside>
 
-            <div></div>
+            <Map
+                center={[-22.8984365,-43.4502295]}
+                zoom={14}
+                style={{ width:'100%', height:'100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
