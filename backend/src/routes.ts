fc094fe290e40440372617a0e0d9359d3e644159
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-10-13 14:18:48 -0300

    cadastro de images no banco de dados

---
+++ backend/src/routes.ts
@@ -0,0 +1,15 @@
+import orphanagesController from './Controllers/OrphanagesController'
+import uploadConfig from './config/upload'
+
+import { Router} from 'express'
+import multer from 'multer'
+
+const routes = Router()
+const upload = multer(uploadConfig)
+
+routes.get('/orphanages', orphanagesController.index )
+routes.get('/orphanages/:id', orphanagesController.show)
+routes.post('/orphanages',upload.array('images') ,orphanagesController.create )
+
+
+export default routes
