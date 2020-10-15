9bdded305852c979d63a5892b7dcd760ccc7b7a0
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-10-15 15:59:09 -0300

    configurando rota

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,79 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions,TouchableOpacity } from 'react-native';
-import MapView,{ Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
-import { Feather } from '@expo/vector-icons'
-
-import mapMarker from './src/images/map-marker.png'
+import { useFonts } from 'expo-font'
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView 
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -22.8845505,
-          longitude: -43.4098808,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }} >
-          <Marker
-            icon={mapMarker}
-            calloutAnchor={{
-              x: 2.7,
-              y: 0.8,
-            }}
-            coordinate={{
-              latitude: -22.8845505,
-              longitude: -43.4098808
-            }}
-          >
-            <Callout tooltip onPress={()=>{ } }>
-              <View style={styles.calloutContainer}>
-                <Text style={styles.calloutText}>Lar das meninas</Text>
-              </View>
-            </Callout>
-          </Marker>
-      </MapView>
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  })
 
-      <View style={styles.footer}>
-        <Text style={styles.footerText}> 2 orfanatos encontrados</Text>
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
-            <Feather name='plus' size={20} color="#fff"/>
-        </TouchableOpacity>
-      </View>
-    </View>
+  if(!fontsLoaded){
+    return null
+  }
+
+  return (
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map:{
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  calloutContainer:{
-    width: 160,
-    height: 46,
-    paddingHorizontal: 16,
-    backgroundColor: 'rgba(255,255,255,0.8)',
-    borderRadius: 16,
-    justifyContent: "center",
-  },
-  calloutText:{
-    color: '#0089a5',
-    fontSize: 14
-  },
-  footer:{
-    
-  },
-  footerText:{
 
-  },
-  createOrphanageButton:{
-    
-  }
-});
