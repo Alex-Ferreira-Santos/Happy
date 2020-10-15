09eadc3dd938389833635f47faecbab0c3f4be18
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-10-15 14:04:03 -0300

    criação da estrutura

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello Nlw</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title:{
+    color:'#fff',
+    fontSize:40
+  }
+});
